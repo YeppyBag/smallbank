a0c53cc1654cc91cf8794cc476bd96de225088ed
Qu4rtzer <Apisit.le@nsru.ac.th>
2024-09-27 19:53:04 +0700

    transfer

---
+++ form/transfer.php
@@ -0,0 +1,26 @@
+<?php
+    include("../connect.inc.php");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <input type="hidden" id="isLoggedIn" value="<?php echo isset($_SESSION['user_id']) ? 'false' : 'true'; ?>" />
+    <script src="../script/login.js"></script>
+    <title>Transfer</title>
+</head>
+<body>
+    <div class="container">
+        <form action="../action/transfer.php" method="POST">
+            <input type="text" name="receiver" placeholder="Receiver Username"><br>
+            <input type="number" name="balance" placeholder="Amount"><br>
+            <input type="hidden" name="sender" value="<?php echo $_SESSION['user_id']?>">
+            <input type="submit" value="Send">
+            <input type="reset" value="Cancel">
+        </form>
+    </div>
+</body>
+</html>
