252bf22df998cfede43fed7743a03f7e593e94e6
Qu4rtzer <Apisit.le@nsru.ac.th>
2024-09-27 18:32:03 +0700

    css

--- form/deposit.php
+++ form/deposit.php
@@ -9,14 +9,17 @@ include "../connect.inc.php";
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/redirect.css">
+    <link rel="stylesheet" href="../css/deposit.css">
     <title>Deposit</title>
 </head>
 <body>
-<form name="disposit_money" method="post" action="../action/deposit.php">
-    <input type="number" name="amount" placeholder="ใส่จำนวนเงิน"><br>
-    <input type="hidden" value="<?php echo $_SESSION['user_id'] ?>" name="user_id">
-    <input type="submit" value="deposit">
-    <input type="reset" value="cancel">
-</form>
+<div class="container">
+    <form name="disposit_money" method="post" action="../action/deposit.php">
+        <input type="number" name="amount" placeholder="ใส่จำนวนเงิน"><br>
+        <input type="hidden" value="<?php echo $_SESSION['user_id'] ?>" name="user_id">
+        <input type="submit" value="deposit"><br>
+        <input type="reset" value="cancel">
+    </form>
+</div>
 </body>
 </html>
--- form/withdraw.php
+++ form/withdraw.php
@@ -9,14 +9,17 @@ include "../connect.inc.php";
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/redirect.css">
+    <link rel="stylesheet" href="../css/deposit.css">
     <title>Withdraw</title>
 </head>
 <body>
-<form name=withdraw_money" method="post" action="../action/withdraw.php">
-    <input type="number" name="amount" placeholder="ใส่จำนวนเงิน"><br>
-    <input type="hidden" value="<?php echo $_SESSION['user_id'] ?>" name="user_id">
-    <input type="submit" value="withdraw">
-    <input type="reset" value="cancel">
-</form>
+<div class="container">
+    <form name=withdraw_money" method="post" action="../action/withdraw.php">
+        <input type="number" name="amount" placeholder="ใส่จำนวนเงิน"><br>
+        <input type="hidden" value="<?php echo $_SESSION['user_id'] ?>" name="user_id">
+        <input type="submit" value="withdraw"><br>
+        <input type="reset" value="cancel">
+    </form>
+</div>
 </body>
 </html>
