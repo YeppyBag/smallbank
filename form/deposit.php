843798f79fa928bc6bca85c6451aaff9c927b13c
Qu4rtzer <Apisit.le@nsru.ac.th>
2024-09-27 16:37:06 +0700

    deposit withdraw

---
+++ form/deposit.php
@@ -0,0 +1,20 @@
+<?php
+    include "../connect.inc.php";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Deposit</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="deposit" id="" placeholder="deposit"><br>
+        <input type="submit" value="deposit">
+        <input type="reset" value="cancel">
+    </form>
+</body>
+</html>
