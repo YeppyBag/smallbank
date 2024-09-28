4873de94e81d1b08bdac2479b258e41293983d1c
chai <chainot@hotmail.co.th>
2024-09-29 04:00:57 +0700

    เช็ค โอนเงิน อันใหม่

--- action/check_transaction.php
+++ action/check_transaction.php
@@ -38,41 +38,70 @@ if (!empty($_SESSION['user_id']) && isset($_POST['user_id']) && isset($_POST['am
         $receiver_username = $row['username'];
         $fee = new Fee($conn);
         $amountfee = $amount * $fee->getSenderFee();
+        $subtotol = $amountfee + $amount;
 
-        // Display the confirmation form
         ?>
-        <!doctype html>
         <html lang="th">
         <head>
             <link rel="stylesheet" href="../css/confirm.css">
             <link rel="stylesheet" href="../css/profile.css">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+            </style>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Confirm Transfer</title>
         </head>
         <body>
         <div class="container">
-            <h2>Confirm Transfer</h2>
-            <div class="confirmation-details">
-                <div class="profile">
-                    <div class="profile-image" style="background-image: url('<?php echo "../" . $sender->getProfile(); ?>');"></div>
-                    <div class="arrow-icon">→</div>
-                    <div class="profile-image" style="background-image: url('<?php echo "../" . $receiver->getProfile(); ?>');"></div>
+            <div class="card cart">
+                <label class="title">ตรวจสอบ</label>
+                <div class="steps">
+                    <div class="step">
+                        <div class="profile">
+                            <div class="profile-image" style="background-image: url('<?php echo "../" . $sender->getProfile(); ?>');"></div>
+                            <div class="arrow-icon">→</div>
+                            <div class="profile-image" style="background-image: url('<?php echo "../" . $receiver->getProfile(); ?>');"></div>
+                        </div>
+                        <hr />
+                        <div class="transaction-info">
+                            <span>TRANSACTION INFO</span>
+                            <div class="transaction-details">
+                                <p class="sender">ผู้โอน: mrchai</p>
+                                <p class="receiver">ผู้รับ: Qu4rtzer</p>
+                            </div>
+                        </div>
+                        <hr />
+                        <div class="payments">
+                            <span>PAYMENT</span>
+                            <div class="details">
+                                <span>จำนวนเงินทั้งหมด:</span>
+                                <span><?= htmlspecialchars($subtotol) ?> บาท</span>
+                                <span>ค่าธรรมเนียม:</span>
+                                <span><?= htmlspecialchars($amountfee) ?> บาท</span>
+                                <span>Fee:</span>
+                                <span><?= $fee->getSenderFee() ?></span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <p><strong>จาก:</strong> <?= htmlspecialchars($sender->getUsername()) ?></p>
-                <p><strong>ไปยัง:</strong> <?= htmlspecialchars($receiver_username) ?></p>
-                <p><strong>จำนวนเงิน:</strong> <?= htmlspecialchars($amount) ?></p>
-                <p><strong>ค่าธรรมเนียม:</strong> <?= htmlspecialchars($amountfee) ?></p> <!-- Adjust this as necessary -->
             </div>
-            <form method="POST" action="../action/money_transaction.php">
-                <input type="hidden" name="user_id" value="<?= htmlspecialchars($sender->getId())?>">
-                <input type="hidden" name="amount" value="<?= htmlspecialchars($amount) ?>">
-                <input type="hidden" name="receiver_id" value="<?= htmlspecialchars($receiver_id) ?>">
-                <input type="hidden" name="transaction_type_id" value="<?= htmlspecialchars($transaction_type_id) ?>" > <!-- Send in tb_transaction_type 2 -->
-                <input type="submit" value="ยืนยัน" class="btn-confirm">
-                <a href="../form/transfer.php" class="btn-cancel">ยกเลิก</a>
-            </form>
+
+            <div class="card checkout">
+                <div class="footer">
+                    <label class="price"><?= htmlspecialchars($subtotol) ?> บาท</label>
+                    <form method="POST" action="../action/money_transaction.php">
+                        <input type="hidden" name="user_id" value="<?= htmlspecialchars($sender->getId())?>">
+                        <input type="hidden" name="amount" value="<?= htmlspecialchars($amount) ?>">
+                        <input type="hidden" name="receiver_id" value="<?= htmlspecialchars($receiver_id) ?>">
+                        <input type="hidden" name="transaction_type_id" value="<?= htmlspecialchars($transaction_type_id) ?>" > <!-- Send in tb_transaction_type 2 -->
+                        <input type="submit" value="ยืนยัน" class="checkout-btn">
+                    </form>
+                </div>
+            </div>
         </div>
         </body>
         </html>
--- form/transfer.php
+++ form/transfer.php
@@ -14,6 +14,12 @@ require_once "../common/User.php";
     <link rel="stylesheet" href="../css/transfer.css">
     <link rel="stylesheet" href="../css/profile.css">
     <link rel="stylesheet" href="../css/nav.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    </style>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -43,14 +49,14 @@ $user = new User($conn, $_SESSION['user_id']);
         FeatureUtil::displayMessage('error', $_GET['transfer-error'] ?? null);
         ?>
         <div class="transfer-method">
-            <label>
+            <label class="radio">
                 <input type="radio" name="transfer_type" value="username" checked
                        onclick="updatePlaceholder('username')">
-                By Username
+                <span class="name">By Username</span>
             </label>
-            <label>
+            <label class="radio">
                 <input type="radio" name="transfer_type" value="email" onclick="updatePlaceholder('email')">
-                By Email
+                <span class="name">By Email</span>
             </label>
         </div>
         <input type="text" id="receiver" name="receiver" placeholder="Receiver Username" required><br>
