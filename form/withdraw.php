750b10f5596bb69db3f41726cd546f5a6b375e8c
chai <chainot@hotmail.co.th>
2024-09-29 00:28:11 +0700

    โชว์จำนวนเงินที่เหลือ กับ แถวกลับหน้าหลัก

--- form/withdraw.php
+++ form/withdraw.php
@@ -1,8 +1,10 @@
 <?php
 use common\FeatureUtil;
+use common\User;
 
+require_once "../common/FeatureUtil.php";
+require_once "../common/User.php";
 include "../connect.inc.php";
-include "../common/FeatureUtil.php";
 ?>
 <!doctype html>
 <html lang="th">
@@ -11,6 +13,7 @@ include "../common/FeatureUtil.php";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="../css/redirect.css">
+    <link rel="stylesheet" href="../css/nav.css">
     <link rel="stylesheet" href="../css/deposit.css">
     <input type="hidden" id="isLoggedIn" value="<?php echo isset($_SESSION['user_id']) ? 'false' : 'true'; ?>" />
     <script src="../script/Login.js"></script>
@@ -19,13 +22,21 @@ include "../common/FeatureUtil.php";
 </head>
 <body>
 <div class="container">
+    <div class="nav">
+        <a href="../index.php">Home</a>
+    </div>
     <h2>ถอนเงิน</h2>
     <form name="withdraw_money" method="post" action="../action/money_transaction.php" onsubmit="confirmAction(event, 'Withdraw')">
         <?php
+        $user_id = $_SESSION['user_id'];
         FeatureUtil::displayMessage('handle', $_GET['withdraw-handle'] ?? null);
         FeatureUtil::displayMessage('error', $_GET['withdraw-error'] ?? null);
+        $user = new User($conn, $user_id);
         ?>
-        <input type="number" name="amount" placeholder="Enter Amount" required>
+        <div class="balance">
+            <p>ยอดเงินที่ทำการได้: ฿<?php echo number_format($user->getWalletBalance(), 2); ?></p> <!-- Display current balance -->
+        </div>
+        <input type="number" name="amount" placeholder="จำนวนที่ต้องการถอน" required>
         <input type="hidden" value="<?php echo $_SESSION['user_id'] ?>" name="user_id">
         <input type="hidden" value="4" name="transaction_type_id"> <!--  Withdraw in tb_transaction_type 4 -->
         <input type="submit" value="ถอน">
