a560181a64473ce6b215fa6c979a2070c8475faf
chai <chainot@hotmail.co.th>
2024-09-28 20:52:13 +0700

    Method เพิ่ม

--- common/User.php
+++ common/User.php
@@ -15,12 +15,9 @@ class User {
     }
 
     public function initWalletBalance() {
-        $sql = "SELECT wallet_balance FROM {$this->table_name} WHERE user_id = $this->userId";
+        $sql = "SELECT wallet_balance FROM $this->table_name WHERE user_id = $this->userId";
         $result = $this->conn->query($sql);
-        if ($row = mysqli_fetch_assoc($result)) {
-            $this->walletBalance = $row['wallet_balance'];
-            return $this->walletBalance;
-        }
+        if ($row = mysqli_fetch_assoc($result)) return $this->walletBalance = $row['wallet_balance'];
         return null;
     }
 
@@ -28,7 +25,31 @@ class User {
         return $this->walletBalance;
     }
 
-    public function getUserId() {
+    public function getId() {
         return $this->userId;
+    }
+    public function getProfile() {
+        $sql = "SELECT profile FROM $this->table_name WHERE user_id = $this->userId";
+        $result = $this->conn->query($sql);
+        if ($row = mysqli_fetch_assoc($result)) return $row['profile'];
+        return null;
+    }
+    public function getEmail() {
+        $sql = "SELECT email FROM $this->table_name WHERE user_id = $this->userId";
+        $result = $this->conn->query($sql);
+        if ($row = mysqli_fetch_assoc($result)) return $row['email'];
+        return null;
+    }
+    public function getUsername() {
+        $sql = "SELECT username FROM $this->table_name WHERE user_id = $this->userId";
+        $result = $this->conn->query($sql);
+        if ($row = mysqli_fetch_assoc($result)) return $row['username'];
+        return null;
+    }
+    public function getUserInfo() {
+        $sql = "SELECT * FROM $this->table_name WHERE user_id = $this->userId";
+        $result = $this->conn->query($sql);
+        if ($row = mysqli_fetch_assoc($result)) return $row;
+        return null;
     }
 }
