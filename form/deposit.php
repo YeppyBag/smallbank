e7b915df458f35c2e91c125446408a2c710b0f0d
chai <chainot@hotmail.co.th>
2024-09-28 20:48:55 +0700

    new deposit

--- form/deposit.php
+++ form/deposit.php
@@ -1,33 +1,43 @@
 <?php
 
 use common\FeatureUtil;
+use common\User;
 
-include "../common/FeatureUtil.php";
+require_once "../common/FeatureUtil.php";
+require_once "../common/User.php";
 include "../connect.inc.php";
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="th">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/redirect.css">
     <link rel="stylesheet" href="../css/deposit.css">
-    <input type="hidden" id="isLoggedIn" value="<?php echo isset($_SESSION['user_id']) ? 'false' : 'true'; ?>" />
+    <input type="hidden" id="isLoggedIn" value="<?php echo isset($_SESSION['user_id']) ? 'false' : 'true'; ?>"/>
     <script src="../script/login.js"></script>
     <title>Deposit</title>
 </head>
 <body>
-<form name="deposit_money" method="post" action="../action/money_transaction.php">
-    <?php
-    FeatureUtil::displayMessage('handle', $_GET['deposit-handle'] ?? null);
-    FeatureUtil::displayMessage('error', $_GET['deposit-error'] ?? null);
-    ?>
-    <input type="number" name="amount" placeholder="ใส่จำนวนเงิน" required max="5000" min="0"><br>
-    <input type="hidden" value="<?php echo $_SESSION['user_id'] ?>" name="user_id">
-    <input type="hidden" value="3" name="transaction_type_id"> <!--  ถอนเงิน ใน tb_transaction_type 3 - deposit  -->
-    <input type="submit" value="ฝาก">
-</form>
+<div class="container">
+    <h2>ฝากเงิน</h2>
+    <form name="deposit_money" method="post" action="../action/money_transaction.php" onsubmit="confirmDeposit(event)">
+        <?php
+        $user_id = $_SESSION['user_id'];
+        FeatureUtil::displayMessage('handle', $_GET['deposit-handle'] ?? null);
+        FeatureUtil::displayMessage('error', $_GET['deposit-error'] ?? null);
+
+        $user = new User($conn, $user_id);
+        ?>
+        <div class="balance">
+            <p>ยอดเงินคงเหลือ: ฿<?php echo number_format($user->getWalletBalance(), 2); ?></p> <!-- Display current balance -->
+        </div>
+        <input type="number" name="amount" placeholder="ใส่จำนวนเงิน" required max="5000" min="0"><br>
+        <input type="hidden" value="<?php echo $_SESSION['user_id'] ?>" name="user_id">
+        <input type="hidden" value="3" name="transaction_type_id"> <!--  Deposit in tb_transaction_type 3 -->
+        <input type="submit" value="ฝาก">
+    </form>
+</div>
 </body>
 </html>
