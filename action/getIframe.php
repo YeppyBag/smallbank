07ab9f3536e14d269240533e42d45e3a48256066
Qu4rtzer <Apisit.le@nsru.ac.th>
2024-09-28 10:18:09 +0700

    switch case getIframe

--- action/getIframe.php
+++ action/getIframe.php
@@ -1,16 +1,31 @@
 <?php
-    include "../connect.inc.php";
-    $page = $_GET['id'];
+include "../connect.inc.php";
+$page = $_GET['id'];
 
-    if($page == 1){
+switch ($page) {
+    case 1:
         $_SESSION['page'] = "form/deposit.php";
         $_SESSION['nameIframe'] = "ฝากเงิน";
         header('location:../index.php');
-    }else {
+        break;
+    case 2:
         $_SESSION['page'] = "form/withdraw.php";
         $_SESSION['nameIframe'] = "ถอนเงิน";
         header('location:../index.php');
-    }
+        break;
+    case 3:
+        $_SESSION['page'] = "form/withdraw.php";
+        $_SESSION['nameIframe'] = "ถอนเงิน";
+        header('location:../index.php');
+        break;
+    case 4:
+        $_SESSION['page'] = "form/withdraw.php";
+        $_SESSION['nameIframe'] = "ถอนเงิน";
+        header('location:../index.php');
+        break;
+    default:
+
+}
 
 
 ?>
