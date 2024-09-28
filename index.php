075128b213cab7da2fadb226955195430aab786d
Qu4rtzer <Apisit.le@nsru.ac.th>
2024-09-28 12:08:01 +0700

    js

--- index.php
+++ index.php
@@ -13,75 +13,69 @@ require_once "common/User.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/index.css">
+
     <title>Small Bank</title>
 </head>
 
 <body>
-<div class="container2">
-    <div class="nav">
-        <?php
-        if (!empty($_SESSION['user_id'])) {
-            echo "<a href='action/logout.php'>logout</a>";
-        } else {
-            echo "<a href='form/login.php'>login</a>";
-        }
-        ?>
-    </div>
-</div>
-<div class="container">
-    <div class="balance">
-        <div class="name">
+    <div class="container2">
+        <div class="nav">
             <?php
             if (!empty($_SESSION['user_id'])) {
-                echo "<H1>" . $_SESSION['username'] . "</H1>";
-            } else
-                echo "<h1>Welcome</h1>";
-            ?>
-        </div>
-        <div class="wallet">
-            <?php
-            if (!empty($_SESSION['user_id'])) {
-                $id = $_SESSION['user_id'];
-                $user = new User($conn, $id);
-                echo "<h1>" . $user->getWalletBalance() . "</h1>";
+                echo "<a href='action/logout.php'>logout</a>";
+            } else {
+                echo "<a href='form/login.php'>login</a>";
             }
             ?>
         </div>
-        <?php
-        if (!empty($_SESSION['user_id'])) {
-            echo "<a href='action/getIframe.php?id=1'>ฝากเงิน</a><a href='action/getIframe.php?id=2'>ถอนเงิน</a><br><br><br><a href='action/getIframe.php?id=3'>โอนเงิน</a><a href='action/getIframe.php?id=4'>Point</a>";
-        }
-        ?>
     </div>
-    <div class="quick">
-        <?php //TODO: Iframe ใน หน้า index ดูไม่ค่อย work ต้อง คอย refresh เพื่อให้ update element ?>
-        <div class="name">
-            <?php
-            if (!empty($_SESSION['nameIframe'])) {
-                echo "<h1>" . $_SESSION['nameIframe'] . "</h1>";
-            } else
-                echo "";
-            ?>
+    <div class="container">
+        <div class="balance">
+            <div class="name">
+                <?php
+                if (!empty($_SESSION['user_id'])) {
+                    echo "<H1>" . $_SESSION['username'] . "</H1>";
+                } else
+                    echo "<h1>Welcome</h1>";
+                ?>
+            </div>
+            <div class="wallet">
+                <?php
+                if (!empty($_SESSION['user_id'])) {
+                    $id = $_SESSION['user_id'];
+                    $user = new User($conn, $id);
+                    echo "<h1>" . $user->getWalletBalance() . "</h1>";
+                }
+                ?>
+            </div>
+            <button onclick="Iframe(1)">ฝากเงิน</button>
+            <button onclick="Iframe(2)">ถอน</button>
         </div>
-        <iframe src="<?php if (empty($_SESSION['page'])) {
-            echo "";
-        } else {
-            echo $_SESSION['page'];
-        } ?>" frameborder="0"></iframe>
-    </div>
-    <div class="transac">
-        <div class="document">
-            <?php
-            if (!empty($_SESSION['user_id'])) {
-                $transaction = new Transaction($conn, $_SESSION['user_id']);
-                $transactionData = $transaction->getTransactionByUserId($_SESSION['user_id']);
-                // ตรวจสอบว่ามีธุรกรรมหรือไม่
-                if (!empty($transactionData)) {
-                    echo "<table border='1'>";
-                    echo "<tr>
+        <div class="quick">
+            <?php //TODO: Iframe ใน หน้า index ดูไม่ค่อย work ต้อง คอย refresh เพื่อให้ update element ?>
+            <div class="name">
+                <?php
+                if (!empty($_SESSION['nameIframe'])) {
+                    echo "<h1>" . $_SESSION['nameIframe'] . "</h1>";
+                } else
+                    echo "";
+                ?>
+            </div>
+            <iframe id="iframe" src="" frameborder="0"></iframe>
+        </div>
+        <div class="transac">
+            <div class="document">
+                <?php
+                if (!empty($_SESSION['user_id'])) {
+                    $transaction = new Transaction($conn, $_SESSION['user_id']);
+                    $transactionData = $transaction->getTransactionByUserId($_SESSION['user_id']);
+                    // ตรวจสอบว่ามีธุรกรรมหรือไม่
+                    if (!empty($transactionData)) {
+                        echo "<table border='1'>";
+                        echo "<tr>
                         <th>Transaction ID</th>
                         <th>User ID</th>
                         <th>Transaction Type ID</th>
@@ -91,9 +85,9 @@ require_once "common/User.php";
                         <th>Created At</th>
                       </tr>";
 
-                    // ใช้ foreach เพื่อแสดงข้อมูลธุรกรรม
-                    foreach ($transactionData as $transaction) {
-                        echo "<tr>
+                        // ใช้ foreach เพื่อแสดงข้อมูลธุรกรรม
+                        foreach ($transactionData as $transaction) {
+                            echo "<tr>
                             <td>{$transaction['transaction_id']}</td>
                             <td>{$transaction['user_id']}</td>
                             <td>{$transaction['transaction_type_id']}</td>
@@ -102,18 +96,30 @@ require_once "common/User.php";
                             <td>{$transaction['recipient_user_id']}</td>
                             <td>{$transaction['created_at']}</td>
                           </tr>";
+                        }
+
+                        echo "</table>";
+                    } else {
+                        echo "ไม่มีข้อมูลธุรกรรม";
                     }
-
-                    echo "</table>";
-                } else {
-                    echo "ไม่มีข้อมูลธุรกรรม";
                 }
-            }
-            ?>
+                ?>
+            </div>
         </div>
-    </div>
 
-</div>
+    </div>
+    <script>
+        function Iframe(page) {
+            switch (page) {
+                case 1: document.getElementById("iframe").src = "form/deposit.php";
+                    break;
+                case 2: document.getElementById("iframe").src = "form/withdraw.php";
+                    break;
+                default:
+                    break;
+            }
+        }
+    </script>
 </body>
 
 </html>
