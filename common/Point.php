7336118492a26e34bfbc826f19b4bb37ab70f653
chai <chainot@hotmail.co.th>
2024-09-29 19:47:17 +0700

    Point.php

--- common/Point.php
+++ common/Point.php
@@ -12,30 +12,47 @@ class Point {
     }
 
     public function addPoints($amount) {
-        $query = "UPDATE tb_user SET points = points + $amount WHERE user_id = $this->userId";
-        if (mysqli_query($this->conn, $query)) {
-            return true;
-        }
-        return false;
+        $updatePoints = "UPDATE tb_point SET points = points + $amount WHERE user_id = $this->userId";
+        $this->executeQuery($updatePoints);
+        $logTransaction = "INSERT INTO tb_point_transaction (user_id, point_amount, transaction_type) 
+                           VALUES ($this->userId, $amount, 'earn')";
+        $this->executeQuery($logTransaction);
     }
 
     public function usePoints($amount) {
         $currentPoints = $this->getPoints();
         if ($currentPoints >= $amount) {
-            $query = "UPDATE tb_user SET points = points - $amount WHERE user_id = $this->userId";
-            if (mysqli_query($this->conn, $query)) {
-                return true;
-            }
+            $updatePoints = "UPDATE tb_point SET points = points - $amount WHERE user_id = $this->userId";
+            $this->executeQuery($updatePoints);
+
+            $logTransaction = "INSERT INTO tb_point_transaction (user_id, point_amount, transaction_type) 
+                               VALUES ($this->userId, $amount, 'use')";
+            $this->executeQuery($logTransaction);
+            return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
     public function getPoints() {
-        $query = "SELECT points FROM tb_user WHERE user_id = $this->userId";
-        $result = mysqli_query($this->conn, $query);
+        $query = "SELECT points FROM tb_point WHERE user_id = $this->userId";
+        $result = $this->executeQuery($query);
         if ($row = mysqli_fetch_assoc($result)) {
             return $row['points'];
         }
         return 0;
+    }
+    public function getTransactionHistory() {
+        $query = "SELECT * FROM tb_point_transaction WHERE user_id = $this->userId ORDER BY created_at DESC";
+        $result = mysqli_query($this->conn, $query);
+
+        $transactions = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $transactions[] = $row;
+        }
+        return $transactions;
+    }
+    private function executeQuery($query) {
+        return mysqli_query($this->conn, $query);
     }
 }
