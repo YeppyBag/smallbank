18cd454d77e70cbc6aca3edbe3379e2fb6448f3c
chai <chainot@hotmail.co.th>
2024-10-01 06:28:35 +0700

    User Check

--- action/register.php
+++ action/register.php
@@ -10,15 +10,23 @@ if ($password !== $cpassword) {
     header("Location: ../form/login.php?register-error=Passwords do not match.");
     exit();
 }
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO tb_user (username, email, password) 
-VALUES ('$username', '$email', '$hashed_password')";
+$sql_check = "SELECT * FROM tb_user WHERE username = '$username'";
+$result_check = mysqli_query($conn, $sql_check);
+
+if (mysqli_num_rows($result_check) > 0) {
+    header("Location: ../form/login.php?register-error=Username already exists.");
+    exit();
+}
+
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+$sql = "INSERT INTO tb_user (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
 $query = mysqli_query($conn, $sql);
 
 if ($query) {
     header("Location: ../form/login.php?register-handle=Successfully registered.");
     exit();
 } else {
-    echo "Error registering user: ";
+    echo "Error registering user: " . mysqli_error($conn);
 }
+?>
