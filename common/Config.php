cf7ebdbcd79e5e4480b06a389659e248e36255b8
chai <chainot@hotmail.co.th>
2024-10-02 19:54:06 +0700

    Config

--- common/Config.php
+++ common/Config.php
@@ -3,4 +3,5 @@ class Config {
     public static float $pointGain = 0.01;
     public static float $extraPointGain = 0.02;
     public static float $reachGainPoint = 1000;
+    public static int $pointRequirement = 1000;
 }
